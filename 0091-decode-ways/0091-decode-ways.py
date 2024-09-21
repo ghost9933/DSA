7e273fe0fbc03eb8e1e417ba4aec545f19925c60
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-09-21 10:39:23 -0500

    Time: 31 ms (86.5%), Space: 16.5 MB (52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,18 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n=len(s)
-        @lru_cache
-        def ways(i):
-            
-            if i==n:
-                return 1
-            if s[i]=='0':
-                return 0
-            if i==n-1:
-                return 1
-            
-            ans=ways(i+1)
-            if int(s[i:i+2])<=26:
-                ans+=ways(i+2)
-            return ans 
-        return ways(0)
+        one=1
+        two=1
+        if s[0]=='0':
+            return 0
+        for i in range(1,len(s)):
+            curr=0
+            if s[i]!='0':
+                curr=one
+            if 10<=int(s[i-1:i+1])<=26:
+                curr+=two
+            two=one
+            one=curr
+        return one
