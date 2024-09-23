f7403999d11f345251f15fc6b6a918bd01dcba08
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-09-22 22:43:14 -0500

    Time: 65 ms (86.07%), Space: 17.1 MB (8.43%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -6,9 +6,9 @@ class Solution:
                 students[item[0]].append(-item[1])
             else:
                 students[item[0]]=[-item[1]]
-        # 
-        for x in students:
-            print(x,students[x])
+        # # 
+        # for x in students:
+        #     # print(x,students[x])
         ans=[]
         def topKavg(heap,k):
             sum=0
@@ -20,13 +20,13 @@ class Solution:
                     break
                 c+=1
                 kth=-heappop(heap)
-                print(c,kth)
+                # print(c,kth)
                 sum+=kth
             if c:
                 return sum//c
             return 0
         for student in students:
-            print('curr student',student)
+            # print('curr student',student)
             x=topKavg(students[student],5)
             ans.append([student,x])
         return sorted(ans,key=lambda x:x[0])
