04f1f0027e5d74e5fd6de0dbd260a030796a26c5
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-09-22 22:42:50 -0500

    Time: 74 ms (31.97%), Space: 17.1 MB (8.43%) - LeetHub

---
+++ 1074-high-five/1074-high-five.py
@@ -0,0 +1,33 @@
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        students={}
+        for item in items:
+            if item[0] in students:
+                students[item[0]].append(-item[1])
+            else:
+                students[item[0]]=[-item[1]]
+        # 
+        for x in students:
+            print(x,students[x])
+        ans=[]
+        def topKavg(heap,k):
+            sum=0
+            c=0
+            heapify(heap)
+            for i in range(k):
+
+                if not heap:
+                    break
+                c+=1
+                kth=-heappop(heap)
+                print(c,kth)
+                sum+=kth
+            if c:
+                return sum//c
+            return 0
+        for student in students:
+            print('curr student',student)
+            x=topKavg(students[student],5)
+            ans.append([student,x])
+        return sorted(ans,key=lambda x:x[0])
+            
