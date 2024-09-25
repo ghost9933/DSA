91066a06d02a8e01f72e95af4e6aedccd56bcd00
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-09-24 21:40:07 -0500

    Time: 1010 ms (31.55%), Space: 35.1 MB (15.87%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.py
@@ -12,13 +12,13 @@ class Solution:
                 root=root[i]
                 root['end2']=1
             root['end2']=1
-        arr1=sorted(arr1,reverse=True)
+        # arr1=sorted(arr1,reverse=True)
         for num in arr1:
             c=0
             root=ptree
             s=str(num)
-            if ans>len(s):
-                break
+            # if ans>len(s):
+            #     break
             for i in s:
                 if i in root and 'end2' in root[i]:
                     c+=1
@@ -30,9 +30,6 @@ class Solution:
                 #     root[i]={}
                 root=root[i]
             ans=max(c,ans)
-            root['end1']=1
+            # root['end1']=1
             
         return ans
-
-        print(ptree)
-        print()
