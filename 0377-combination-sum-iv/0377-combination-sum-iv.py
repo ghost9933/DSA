511546eb95923cf04ef928464d016be0bb7a139e
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-09-30 23:15:44 -0500

    Time: 41 ms (52.93%), Space: 16.8 MB (11.96%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+
+        #  brute force 2^n
+        map={}
+        res=0
+        end=len(nums)
+        # lru_cache(maxsize = None)
+        def backtrack(r):
+            # print(map)
+            if r==0:
+                return 1
+            if r in map:
+                return map[r]
+            c=0
+            for x in nums:
+                if r-x>=0:
+                    c+=backtrack(r-x)
+            map[r]=c
+            return c
+        return backtrack(target)
+        return res
+            
+            
