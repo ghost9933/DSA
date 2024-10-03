1cd58f59d4063bbca79789d41950bb296cd4a2fe
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-03 00:10:04 -0500

    Time: 8542 ms (5.01%), Space: 16.8 MB (6.51%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,46 +1,53 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.ROWS = len(board)
-        self.COLS = len(board[0])
-        self.board = board
+        v={}
+        # /in case there were n words/
+        # trie={}
+        # for w in word:
+        #     root=trie
+        #     for i in w:
+        #         if i not in root:
+        #             root[i]={}
+        #         root=root[i]
+        #     root['end']=word
+        # print(trie)
+        # for x in trie:
+        #     print(x)
 
-        for row in range(self.ROWS):
-            for col in range(self.COLS):
-                if self.backtrack(row, col, word):
-                    return True
-
-        # no match found after all exploration
-        return False
-
-    def backtrack(self, row: int, col: int, suffix: str) -> bool:
-        """
-        backtracking with side-effect,
-           the matched letter in the board would be marked with "#".
-        """
-        # bottom case: we find match for each letter in the word
-        if len(suffix) == 0:
-            return True
-
-        # Check the current status, before jumping into backtracking
-        if (
-            row < 0
-            or row == self.ROWS
-            or col < 0
-            or col == self.COLS
-            or self.board[row][col] != suffix[0]
-        ):
-            return False
-
-        # mark the choice before exploring further.
-        self.board[row][col] = "#"
-        # explore the 4 neighbor directions
-        for rowOffset, colOffset in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
-            # sudden-death return, no cleanup.
-            if self.backtrack(row + rowOffset, col + colOffset, suffix[1:]):
+        xLimit=len(board)
+        yLimit=len(board[0])
+        deltas=[(0,1),(-1,0),(1,0),(0,-1)]
+        def dfsSearch(rem,curr,visited):
+            # print(curr,rem)
+            nv=[]
+            if rem[0] ==board[curr[0]][curr[1]]:
+                nv=nv+visited+[(curr)]
+            else:
+                return
+            rem=rem[1:]
+            # print(curr,rem,visited)
+            if len(rem)==0:
+                # print('found at',visited,curr)
                 return True
+            ans=False
+            for x,y in deltas:
+                
+                dx=x+curr[0]
+                dy=y+curr[1]
+                # print('check',(dx,dy))
+                
+                if (dx,dy) not in nv and 0<=dx<xLimit and 0<=dy<yLimit:
+                    ans=ans or dfsSearch(rem,(dx,dy),nv[:])
+            return ans 
 
-        # revert the marking
-        self.board[row][col] = suffix[0]
-
-        # Tried all directions, and did not find any match
+        # print(xLimit,yLimit)
+        for i in range(xLimit):
+            # print(i,"_")
+            for j in range(yLimit):
+                # print("_",j)
+                # print('enterning ',(i,j))
+                if board[i][j] ==word[0]:
+                    ispresent=dfsSearch(word,(i,j),[])
+                    if ispresent:
+                        return True
         return False
