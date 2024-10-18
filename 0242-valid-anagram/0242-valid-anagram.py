d7174ed67d0435ba443d688ca2592222f98dca12
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-18 12:29:56 -0500

    Time: 9 ms (100%), Space: 16.6 MB (99.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        xmap={}
+        ymap={}
+        for x in s:
+            if x in xmap:
+                xmap[x]+=1
+            else:
+                xmap[x]=1
+        
+        for y in t:
+            if y in ymap:
+                ymap[y]+=1
+            else:
+                ymap[y]=1
+        # print(xmap,"\n",ymap)
+        if xmap==ymap:
+            return True
+        else :
+            return False
+        
