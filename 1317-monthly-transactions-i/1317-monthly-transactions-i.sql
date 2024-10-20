b9c7496759d13ea2883cffd486d99bc1015ce1bc
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-20 15:58:13 -0500

    Time: 587 ms (55.66%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,56 @@
+-- # Write your MySQL query statement below
+-- with main as (
+--     select *,DATE_FORMAT(trans_date, '%Y-%m') as month  FROM Transactions
+--     ),
+-- global as (
+-- select month, country, count(id) as trans_count ,sum(amount) trans_total_amount from main
+-- group by month,country ),
+-- filtered as (
+--    select month m, country c
+--    , count(id) as approved_count  ,sum(amount) approved_total_amount  
+--    from main
+--    where state='approved '
+-- group by month,country 
+-- )
+-- select  month    ,country,trans_count , IFNULL(approved_count,0) approved_count, trans_total_amount ,ifnull(approved_total_amount,0) approved_total_amount from global left join filtered
+-- on global.month=filtered.m and global.country=filtered.c
+
+
+WITH main AS (
+    SELECT 
+        id,
+        DATE_FORMAT(trans_date, '%Y-%m') AS month,
+        country,
+        amount,
+        state
+    FROM Transactions
+),
+global AS (
+    SELECT 
+        month, 
+        ifnull(country,'NULL') country, 
+        COUNT(id) AS trans_count,
+        SUM(amount) AS trans_total_amount 
+    FROM main
+    GROUP BY month, country
+),
+filtered AS (
+    SELECT 
+        month AS m, 
+        ifnull(country,'NULL') c,
+        COUNT(id) AS approved_count,
+        SUM(amount) AS approved_total_amount  
+    FROM main
+    WHERE state = 'approved'  -- Removed the trailing space
+    GROUP BY month, country 
+)
+SELECT  
+    global.month,
+    if(global.country='NULL',NULL,global.country) as country,
+    global.trans_count,
+    IFNULL(filtered.approved_count, 0) AS approved_count,
+    global.trans_total_amount,
+    IFNULL(filtered.approved_total_amount, 0) AS approved_total_amount 
+FROM global 
+LEFT JOIN filtered
+ON global.month = filtered.m AND global.country = filtered.c;
