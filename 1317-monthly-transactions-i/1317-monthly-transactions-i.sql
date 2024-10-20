1b441d39947731a1ae67fb7790d2f4a0bdb4aa7b
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-20 18:43:40 -0500

    Time: 688 ms (26.07%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -16,41 +16,54 @@
 -- on global.month=filtered.m and global.country=filtered.c
 
 
-WITH main AS (
-    SELECT 
-        id,
-        DATE_FORMAT(trans_date, '%Y-%m') AS month,
-        country,
-        amount,
-        state
-    FROM Transactions
-),
-global AS (
-    SELECT 
-        month, 
-        ifnull(country,'NULL') country, 
-        COUNT(id) AS trans_count,
-        SUM(amount) AS trans_total_amount 
-    FROM main
-    GROUP BY month, country
-),
-filtered AS (
-    SELECT 
-        month AS m, 
-        ifnull(country,'NULL') c,
-        COUNT(id) AS approved_count,
-        SUM(amount) AS approved_total_amount  
-    FROM main
-    WHERE state = 'approved'  -- Removed the trailing space
-    GROUP BY month, country 
-)
-SELECT  
-    global.month,
-    if(global.country='NULL',NULL,global.country) as country,
-    global.trans_count,
-    IFNULL(filtered.approved_count, 0) AS approved_count,
-    global.trans_total_amount,
-    IFNULL(filtered.approved_total_amount, 0) AS approved_total_amount 
-FROM global 
-LEFT JOIN filtered
-ON global.month = filtered.m AND global.country = filtered.c;
+-- WITH main AS (
+--     SELECT 
+--         id,
+--         DATE_FORMAT(trans_date, '%Y-%m') AS month,
+--         country,
+--         amount,
+--         state
+--     FROM Transactions
+-- ),
+-- global AS (
+--     SELECT 
+--         month, 
+--         ifnull(country,'NULL') country, 
+--         COUNT(id) AS trans_count,
+--         SUM(amount) AS trans_total_amount 
+--     FROM main
+--     GROUP BY month, country
+-- ),
+-- filtered AS (
+--     SELECT 
+--         month AS m, 
+--         ifnull(country,'NULL') c,
+--         COUNT(id) AS approved_count,
+--         SUM(amount) AS approved_total_amount  
+--     FROM main
+--     WHERE state = 'approved'  -- Removed the trailing space
+--     GROUP BY month, country 
+-- )
+-- SELECT  
+--     global.month,
+--     if(global.country='NULL',NULL,global.country) as country,
+--     global.trans_count,
+--     IFNULL(filtered.approved_count, 0) AS approved_count,
+--     global.trans_total_amount,
+--     IFNULL(filtered.approved_total_amount, 0) AS approved_total_amount 
+-- FROM global 
+-- LEFT JOIN filtered
+-- ON global.month = filtered.m AND global.country = filtered.c;
+
+
+SELECT 
+    LEFT(trans_date, 7) AS month,
+    country, 
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    month, country;
