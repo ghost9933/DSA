135f527220f09f7e769e844bccba32307d558a8d
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-22 20:41:27 -0500

    Time: 599 ms (60.96%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
+with main as(select *,row_number() over (partition by customer_id order by order_date asc) as rn from Delivery )
 select round(COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END)/count(order_date)*100,2) as immediate_percentage 
-from (
-    select *,row_number() over (partition by customer_id order by order_date asc) as rn from Delivery 
-) main where rn=1
+from  main where rn=1
 
