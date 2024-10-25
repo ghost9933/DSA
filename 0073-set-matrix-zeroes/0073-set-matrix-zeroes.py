d041e01e7dbad1be78e71242f6a2dc9abae6bbe2
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-25 00:21:03 -0500

    Time: 3 ms (97.35%), Space: 17.8 MB (6.48%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -6,27 +6,32 @@ class Solution:
         if not matrix or not matrix[0]:
             return matrix
         
-        rows = len(matrix)
-        cols = len(matrix[0])
-        
-        zero_rows = set()
-        zero_cols = set()
-        
+        rows=len(matrix)
+        cols=len(matrix[0])
         # First pass: find all the rows and columns that need to be zeroed
+        is_col=False
         for i in range(rows):
-            for j in range(cols):
+            if matrix[i][0] == 0:
+                is_col = True
+            for j in range(1,cols):
                 if matrix[i][j] == 0:
-                    zero_rows.add(i)
-                    zero_cols.add(j)
-        
-        # Second pass: zero out the marked rows
-        for i in zero_rows:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        for x in matrix:
+            print(x)
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+
+        # See if the first row needs to be set to zero as well
+        if matrix[0][0] == 0:
             for j in range(cols):
-                matrix[i][j] = 0
-        
-        # Zero out the marked columns
-        for j in zero_cols:
+                matrix[0][j] = 0
+
+        # See if the first column needs to be set to zero as well
+        if is_col:
             for i in range(rows):
-                matrix[i][j] = 0
-        
+                matrix[i][0] = 0
+       
         return matrix
