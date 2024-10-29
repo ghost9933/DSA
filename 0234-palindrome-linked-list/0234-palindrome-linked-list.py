05ef390fd2fc51efdee07eaade5ea4024bff9616
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-29 13:14:02 -0500

    Time: 12 ms (98.48%), Space: 38.7 MB (9.45%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -10,24 +10,24 @@ class Solution:
         while x:
             x=x.next
             n+=1
-        print('len',n)
+        # print('len',n)
         isodd=False
         if n%2!=0:
             isodd=True
         if n==1:
             return True
         m=n//2
-        print(m,'is odd',isodd)
+        # print(m,'is odd',isodd)
         s=[]
         x=head
         for i in range(m):
             s.append(x.val)
             x=x.next
-        print(s)
-        print(x.val)
+        # print(s)
+        # print(x.val)
         if isodd:
             x=x.next
-        print(x.val)
+        # print(x.val)
         while x:
             if x.val!=s[-1]:
                 return False
