c620ac150b4a61e29d1db38050c0fa6c64c81d4e
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-10-29 10:46:54 -0500

    Time: 2 ms (75.16%), Space: 16.7 MB (47.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,37 +5,40 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        def add(l1: Optional[ListNode], l2: Optional[ListNode], carry: int) -> Optional[ListNode]:
-            if not l1 and not l2:
-                if carry == 0:
-                    return None
-                else:
-                    return ListNode(carry)
-            
-            if not l2:
-                if carry == 0:
-                    return l1
-                else:
-                    sum_value = l1.val + carry
-                    l1.val = sum_value % 10
-                    carry = sum_value // 10
-                    l1.next = add(l1.next, None, carry)
-                    return l1
+        x=l1
+        y=l2
+        c=0
+        ans=ListNode()
+        p=ans
+        while x or y:
+            if x and y:
+                ans.val=(x.val+y.val+c)%10
+                c=(x.val+y.val+c)//10
+                if x.next or y.next or c!=0:
+                    ans.next=ListNode()
+                ans=ans.next
+                x=x.next
+                y=y.next
+            elif x:
+                ans.val=(x.val+c)%10
+                c=(x.val+c)//10
+                if x.next or c!=0:
+                    ans.next=ListNode()
+                ans=ans.next
+                x=x.next
+            elif y:
+                ans.val=(y.val+c)%10
+                c=(y.val+c)//10
+                if y.next or c!=0:
+                    ans.next=ListNode()
+                ans=ans.next
+                y=y.next
+            elif c>0:
+                ans.val=c
+            else:
+                return p
+        if c!=0:
+            ans.val=c
+        return p
 
-            if not l1:
-                if carry == 0:
-                    return l2
-                else:
-                    sum_value = l2.val + carry
-                    l2.val = sum_value % 10
-                    carry = sum_value // 10
-                    l2.next = add(l2.next, None, carry)
-                    return l2
-            
-            sum_value = l1.val + l2.val + carry
-            result_node = ListNode(sum_value % 10)
-            carry = sum_value // 10
-            result_node.next = add(l1.next, l2.next, carry)
-            return result_node
-        return add(l1, l2, 0)
+
