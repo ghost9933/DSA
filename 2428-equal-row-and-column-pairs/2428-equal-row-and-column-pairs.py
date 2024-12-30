526f0e413a0f5362cd7b53f607cda78340cf1acf
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2024-12-30 16:52:36 -0600

    Time: 127 ms (29.78%), Space: 22.2 MB (10.62%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.py
@@ -2,15 +2,15 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         rows=[]
         cols=[[] for i in range(len(grid[0]))]
-        print(cols)
+        # print(cols)
         for i in range(len(grid)):
             rows.append(tuple(grid[i]))
             for j in range(len(grid[0])):
                 cols[j].append(grid[i][j])
         for i in range(len(cols)):
             cols[i]=tuple(cols[i])
-        print(rows)
-        print(cols)
+        # print(rows)
+        # print(cols)
         ans=0
         for x in cols:
             for j in rows:
