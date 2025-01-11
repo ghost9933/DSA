78d1c3dccff6efd10fdf00f3bb0ab52374e959f1
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-01-11 00:44:41 -0600

    Time: 2 ms (2.93%), Space: 18 MB (12.68%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def tribonacci(self, n: int) -> int:
+        mapF=dict({0:0,1:1,2:1})
+        def find(n):
+            
+            nonlocal mapF
+            print(n,mapF)
+            if n in mapF:
+                return mapF[n]
+            else :
+                x=find(n-3)+find(n-2)+find(n-1)
+                print(n,x)
+                mapF[n]=x
+                return mapF[n]
+        print(mapF)
+        return find(n)
+        
