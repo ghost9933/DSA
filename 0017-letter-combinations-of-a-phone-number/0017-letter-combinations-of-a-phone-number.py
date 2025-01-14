889ada9d999502cf1209f20f6739c2c2dbb81d2b
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-01-14 14:32:57 -0600

    Time: 0 ms (100%), Space: 17.9 MB (11.25%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        digitMap={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        ans=[]
+        def trav(d,c):
+            nonlocal ans
+            if len(d)<=0:
+                if c!='':
+                    ans.append(c)
+                return
+            chars=digitMap[d[0]]
+            for x in chars:
+                print(x,d[1:],c+x)
+                trav(d[1:],c+x)
+        trav(digits,'')
+        return ans    
