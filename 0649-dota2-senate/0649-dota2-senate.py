415c26112c16617aad27ce9a38bfc1870aac8fad
Nikhil Nandkumar Mhatre <NXM2555@MAVS.UTA.EDU>
2025-01-17 17:36:39 -0600

    Time: 11 ms (79.32%), Space: 17.8 MB (59.67%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,55 +1,39 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
 
-        # Converting to List as string is immutable, and we need to remove.
-        # List will save only eligible senators
-        senate = list(senate)
-
-        # Count of Each Type of Senator to check for Winner
+        # Eligible Senators of each party
         r_count = senate.count('R')
         d_count = len(senate) - r_count
 
-        # Ban the candidate "to_ban", immediate next to "start_at"
-        # If have to loop around, then it means next turn will be of
-        # senator at same index. Returns loop around boolean
-        def ban(to_ban, start_at):
+        # Floating Ban Count
+        d_floating_ban = 0
+        r_floating_ban = 0
 
-            loop_around = False
-            pointer = start_at
+        # Queue of Senators
+        q = deque(senate)
 
-            while True:
-                if pointer == 0:
-                    loop_around = True
-                if senate[pointer] == to_ban:
-                    senate.pop(pointer)
-                    break
-                pointer = (pointer + 1) % len(senate)
+        # While any party has eligible Senators
+        while r_count and d_count:
 
-            return loop_around
+            # Pop the senator with turn
+            curr = q.popleft()
 
-        # Turn of Senator at this index
-        turn = 0
-
-        # While No Winner
-        while r_count > 0 and d_count > 0:
-
-            # Ban the next opponent, starting at one index ahead
-            # Taking MOD to loop around
-            if senate[turn] == 'R':
-                banned_senator_before = ban('D', (turn + 1) % len(senate))
-                d_count -= 1
+            # If eligible, float the ban on the other party, enqueue again.
+            # If not, decrement the floating ban and count of the party.
+            if curr == 'D':
+                if d_floating_ban:
+                    d_floating_ban -= 1
+                    d_count -= 1
+                else:
+                    r_floating_ban += 1
+                    q.append('D')
             else:
-                banned_senator_before = ban('R', (turn + 1) % len(senate))
-                r_count -= 1
+                if r_floating_ban:
+                    r_floating_ban -= 1
+                    r_count -= 1
+                else:
+                    d_floating_ban += 1
+                    q.append('R')
 
-            # If the index of the banned senator is before current index,
-            # then we need to decrement turn by 1, as we have removed
-            # a senator from the list
-            if banned_senator_before:
-                turn -= 1
-
-            # Increment turn by 1
-            turn = (turn + 1) % len(senate)
-
-        # Return Winner depending on the count
-        return 'Radiant' if d_count == 0 else 'Dire'
+        # Return the party with eligible Senators
+        return 'Radiant' if r_count else 'Dire'
